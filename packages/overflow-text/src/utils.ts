a9724c373c2388bbafea218bf44db1448c1af7aa
gumspeaker <57261014+gumspeaker@users.noreply.github.com>
2024-10-12 13:40:15 +0800

    Update utils.ts

--- packages/overflow-text/src/utils.ts
+++ packages/overflow-text/src/utils.ts
@@ -1,1 +1,8 @@
 
+function splited(text:string,keyword:string) {
+    return text.split(new RegExp(`(${escapeRegExp(keyword)})`, 'gi'));
+}
+
+function escapeRegExp(str:string) {
+  return string.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&'); // $& 代表整个匹配项
+}
